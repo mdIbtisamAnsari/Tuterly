219dfd6abcc3b623f3f484fccecdf46122dce63e
mdIbtisamAnsari <173330530+mdIbtisamAnsari@users.noreply.github.com>
2026-06-04 13:50:45 +0530

    changes

--- src/components/portalCards/TeacherCard.tsx
+++ src/components/portalCards/TeacherCard.tsx
@@ -1,15 +1,60 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const TeacherCard: React.FC<{ teacher: any }> = ({ teacher }) => {
+interface Teacher {
+    id: number;
+    name: string;
+    profilePicture: string;
+    subjects: string[];
+    bio: string;
+    rate: number;
+    experience: number;
+}
+
+
+const TeacherCard: React.FC<{ teacher: Teacher }> = ({ teacher }) => {
+
+
     return (
-        <div className='bg-gray-800 m-3 p-5 rounded-2xl min-w-[calc(100vw-6*4px)]'>
-            <img src={teacher.profilePicture} className='w-20'/>
-            <h2 >{teacher.name}</h2>
-            <p>{teacher.subjects.join(', ')}</p>
-            <p>{teacher.bio}</p>
-            <p>{teacher.rate}</p>
-            <p>{teacher.experience}</p>
+        <div className='bg-gray-800 m-3 p-5 rounded-2xl max-sm:w-[calc(100vw-6*4px)] h-fit relative'>
+            <img 
+            src={teacher.profilePicture} 
+            className='w-20 h-20 rounded-full ' />
+
+            <h2 
+            className='font-bold'>
+                {teacher.name}
+            </h2>
+
+            <p 
+            className='flex gap-1 my-2'>
+                {teacher.subjects.map((sub: string) => (
+
+                    <p 
+                    className='bg-gray-700 rounded-full px-2'>
+                        {sub}
+                    </p>
+            ))}
+            </p>
+
+            <p>
+                {teacher.bio.slice(0, 50)}...
+            </p>
+
+            <p 
+            className='bg-gray-600 rounded-full px-2 w-fit mt-5'>
+                {`Rate: ${teacher.rate} Rs /Mh`}
+            </p>
+
+            <p 
+            className='bg-gray-500 rounded-full px-2 w-fit mt-2'>
+                {`Experience: ${teacher.experience} years`}
+            </p>
+
+            <Link to={`/teacher/${teacher.id}`} className='absolute bottom-5 right-5 bg-blue-600 px-3 py-1 rounded-lg'>View Profile</Link>
+            <Link to={`/teacher/${teacher.id}/mail`} className='absolute bottom-15 right-5 bg-green-600 px-3 py-1 rounded-lg'>Send mail</Link>
+
         </div>
     )
 }
--- src/pages/portals/PortalForStudents.tsx
+++ src/pages/portals/PortalForStudents.tsx
@@ -4,19 +4,49 @@ import TeacherCard from '../../components/portalCards/TeacherCard'
 
 const PortalForTeacher: React.FC = () => {
 
-    const [teachers, setTeachers] = useState<any[]>([])
+    interface Teacher {
+        id: number;
+        name: string;
+        profilePicture: string;
+        subjects: string[];
+        bio: string;
+        rate: number;
+        experience: number;
+    }
+
+    const [teachers, setTeachers] = useState<Teacher[] | []>([])
 
     useEffect(() => {
         axios.get('/api/teachers')
             .then(data => setTeachers(data.data))
             .catch(error => console.error('Error fetching teacher data:', error))
     }, [])
+
+
     return (
-        <div className='min-h-screen'>
-            {teachers.map((teacher) => (
-                <TeacherCard key={teacher.id} teacher={teacher} />
-            ))}
+        <>
+        {/* search */}
+        <div className='justify-center items-center flex relative'>
+            
+            <input type='text' 
+            placeholder='Search Subject..'
+            className='bg-gray-700 px-5 rounded-full text-xl py-2 mt-4'/>
+
         </div>
+        {/* result */}
+        <div className='min-h-[calc(100vh-15rem)]'>
+
+            <div className=' sm:grid sm:grid-cols-2 lg:grid-cols-3 '>
+
+                {teachers.map((teacher: Teacher) => (
+                    <TeacherCard key={teacher.id} teacher={teacher} />
+
+                ))}
+
+            </div>
+
+        </div>
+        </>
     )
 }
 
